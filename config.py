cf09fbe306e44fac735f52abe9e926cccbfede07
meldayazgin <yazginmelda@gmail.com>
2025-02-21 10:46:03 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ MAIL_CONFIG = {
     'MAIL_SERVER': 'smtp.gmail.com',
     'MAIL_PORT': 587,
     'MAIL_USE_TLS': True,
-    'MAIL_USERNAME': 'yazginmelda@gmail.com',
-    'MAIL_PASSWORD': 'whxl pygf ifbz levo',
+    'MAIL_USERNAME': #insert email,
+    'MAIL_PASSWORD': #insert password
 }
